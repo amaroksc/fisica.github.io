11489d5f389311d1d42a11c6c948935a1bd6cba9
amaroksc <53928868+amaroksc@users.noreply.github.com>
2020-12-09 17:11:55 -0400

    mensaje

--- contacto.php
+++ contacto.php
@@ -8,7 +8,7 @@ echo "<h2> Gracias por escribirnos $name <br>";
 echo "Nuestra Empresa pronto se pondrá en contacto<br>";
 
 $message=" Remitente:$name,su email:$email,Comentario: $comment";
-mail("contacto.php","Mensaje del sitio web",$message);
+mail("vdcloayza@gmail.com","Mensaje del sitio web",$message);
 
 
 ?>
