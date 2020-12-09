1b9a7001beabc08cc9eff40110dc9467b7000477
amaroksc <53928868+amaroksc@users.noreply.github.com>
2020-12-09 18:41:17 -0400

    ff

--- contacto.php
+++ contacto.php
@@ -6,8 +6,9 @@ $comment=$_POST['comment'];
 
 echo "<h2> Gracias por escribirnos $name <br>";
 echo "Nuestra Empresa pronto se pondrá en contacto<br>";
+echo "gracias por tu comentario $email<br>";
 
-$message=" Remitente:$name,su email:$email,Comentario: $comment";
+$message=" Remitente: $name,su email:$email,Comentario: $comment";
 mail("vdcloayza@gmail.com","Mensaje del sitio web",$message);
 
 
